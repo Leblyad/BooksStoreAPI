15eb1039fd3c62c87945857e88cea8d24e1c1054
Кирилл Лебедев <apk59955@gmail.com>
2022-04-10 17:41:11 +0300

    Adding logger

--- BooksStore/Startup.cs
+++ BooksStore/Startup.cs
@@ -1,5 +1,7 @@
+using BooksStore.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -7,8 +9,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NLog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,6 +22,8 @@ namespace BooksStore
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+                "/nlog.config"));
             Configuration = configuration;
         }
 
@@ -26,12 +32,11 @@ namespace BooksStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+            services.ConfigureLoggerService();
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BooksStore", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +45,21 @@ namespace BooksStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BooksStore v1"));
+            }
+            else
+            {
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
 
             app.UseRouting();
 
--- BooksStore/WeatherForecast.cs
+++ BooksStore/WeatherForecast.cs
@@ -1,9 +1,32 @@
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 namespace BooksStore
 {
-    public class WeatherForecast
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherForecast : ControllerBase
     {
+        private ILoggerManager _logger;
+
+        public WeatherForecast(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is info message from our values conroller.");
+            _logger.LogDebug("Here is debug message from our values conroller.");
+            _logger.LogWarn("Here is warn message from our values conroller.");
+            _logger.LogError("Here is erro message from our values conroller.");
+
+            return new string[] { "value1", "value2" };
+        }
+
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
